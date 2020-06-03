8c29d82c193c61f683e5397757d5c342f19a9549
Vikrant <vikrantmalik051@gmail.com>
2020-06-03 10:24:04 +0530

    Add test files

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./pclmtoraster 1 1 1 1 "print-color-mode=Srgb" ./pclm/color.jpg-4x6-srgb-8-300dpi.pclm > ./raster/color.jpg-4x6-srgb-8-300dpi.pclm.pwg
+./pclmtoraster 1 1 1 1 "print-color-mode=Srgb" ./pclm/onepage-a4-srgb-8-300dpi.pclm > ./raster/onepage-a4-srgb-8-300dpi.pclm.pwg
