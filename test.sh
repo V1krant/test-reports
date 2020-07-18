9ae63ed421172e5c7bd6962269d923e3ac04cbf9
Vikrant <vikrantmalik051@gmail.com>
2020-07-18 10:30:16 +0530

    Add scanned files

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
-for pclm in pclms/*; do
-	for ppd in ppds/*; do
-		#echo "cupsfilter -o media=A4 -i application/PCLm -m application/vnd.cups-raster -P "$ppd" "$pclm" > rasters/"$(basename "$ppd" .ppd)"_"$(basename "$pclm")".pwg"
-		cupsfilter -o media=A4 -i application/PCLm -m application/vnd.cups-raster -P "$ppd" "$pclm" > rasters/"$(basename "$ppd" .ppd)"_"$(basename "$pclm")".pwg
-		# echo "cupsfilter -o media=A4 -i application/PCLm -m application/vnd.cups-raster -P $(basename "$ppd") $(basename "$pclm")"
-		# cupsfilter $(basename "$filename")
-	done
+#for pclm in pclms/*; do
+#	for ppd in ppds/*; do
+#		cupsfilter -o media=A4 -i application/PCLm -m application/vnd.cups-raster -P "$ppd" "$pclm" > rasters/"$(basename "$ppd" .ppd)"_"$(basename "$pclm")".pwg
+
+#	done
+#done
+for scanned in scanned/*; do
+	cupsfilter -o media=A4 -i application/PCLm -m application/vnd.cups-raster "$scanned" > rasters\(scanned\)/"$(basename "$scanned")".pwg
 done
